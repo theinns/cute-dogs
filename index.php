423fd04664eda084d5949a2ebbe25e2f1894c6bb
theinns <72351418+theinns@users.noreply.github.com>
2020-10-04 19:33:40 +0200

    Update index.php

--- index.php
+++ index.php
@@ -12,10 +12,12 @@
 		<li><a href="Page2.html"> Page 2</a></li>
 		<li><a href="Page3.html"> Page 3</a></li>
 	</ul>
-  echo '<?php include_once("index.html");?>' > index.php
 	<h2>This website is about cute dogs</h2>
 	<h3>And here we have a Corgi</h3>
 	<img src="https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/14112506/Pembroke-Welsh-Corgi-standing-outdoors-in-the-fall.jpg">
 	<p>The Pembroke Welsh Corgi is a cattle herding dog breed that originated in Pembrokeshire, Wales. It is one of two breeds known as a Welsh Corgi. The other is the Cardigan Welsh Corgi, and both descend from the line of northern spitz-type dogs.separate and distinct breed from the Cardigan.</p>
+	<?php
+	echo "My first PHP script!";
+	?>
 </body>
 </html>
